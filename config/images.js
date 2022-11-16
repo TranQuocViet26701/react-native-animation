0d9f42dc8e9990640692c4b458c298731ee9ebb1
viettq <viettq26701@gmail.com>
2022-11-16 23:56:31 +0700

    remove images

--- config/images.js
+++ config/images.js
@@ -1,7 +1,7 @@
 export const Images = {
-  course1: require('../assets/images/course-1.jpeg'),
-  course2: require('../assets/images/course-2.jpeg'),
-  course3: require('../assets/images/course-3.jpeg'),
-  course4: require('../assets/images/course-4.jpeg'),
-  course5: require('../assets/images/course-5.jpeg'),
+  course1: require('../assets/images/course.jpeg'),
+  course2: require('../assets/images/course.jpeg'),
+  course3: require('../assets/images/course.jpeg'),
+  course4: require('../assets/images/course.jpeg'),
+  course5: require('../assets/images/course.jpeg'),
 };
